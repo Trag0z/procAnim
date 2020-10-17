41cf8730a35d9a866c947cbb1ebd891329925455
Trag0z <traegges1@gmail.com>
2020-10-17 11:18:42 +0200

    entity in two files

--- src/Entity.h
+++ src/Entity.h
@@ -9,34 +9,19 @@ class Entity {
 
     const Entity* parent = nullptr;
 
-    void update_model_matrix() {
-        model = glm::translate(glm::mat3(1.0f), position);
-        model = glm::scale(model, scale);
-    }
+    void update_model_matrix();
     void init(glm::vec2 pos_ = glm::vec2(0.0f),
               glm::vec2 scale_ = glm::vec2(1.0f),
-              const Entity* parent_ = nullptr) {
-        position = pos_;
-        scale = scale_;
-        update_model_matrix();
-        parent = parent_;
-    };
+              const Entity* parent_ = nullptr);
 
   public:
-    glm::vec2 world_to_local_space(const glm::vec2& world_pos) const {
-        return glm::inverse(model) * glm::vec3(world_pos, 1.0f);
-    }
-    glm::vec2 local_to_world_space(const glm::vec2& local_pos) const {
-        return model * glm::vec3(local_pos, 1.0f);
-    }
-    glm::vec2 world_to_world_scale(const glm::vec2& world_vec) const {
-        return {world_vec.x / scale.x, world_vec.y / scale.y};
-    }
-    glm::vec2 local_to_world_scale(const glm::vec2& local_vec) const {
-        return {local_vec.x * scale.x, local_vec.y * scale.y};
-    }
+    glm::vec2 world_to_local_space(const glm::vec2& world_pos) const;
+    glm::vec2 local_to_world_space(const glm::vec2& local_pos) const;
 
-    const glm::mat3& get_model_matrix() const { return model; }
+    glm::vec2 world_to_world_scale(const glm::vec2& world_vec) const;
+    glm::vec2 local_to_world_scale(const glm::vec2& local_vec) const;
 
-    glm::vec2 get_position() const { return position; }
+    const glm::mat3& get_model_matrix() const;
+
+    glm::vec2 get_position() const;
 };
